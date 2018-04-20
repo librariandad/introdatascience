f5b349beb829d4b670f8b11dd279b0580cbf025d
Keith Engwall <engwall@oakland.edu>
2018-04-20 11:29:14 -0400

    Completed 7.3 Clustering Project

---
+++ Clustering.R
@@ -0,0 +1,97 @@
+# This mini-project is based on the K-Means exercise from 'R in Action'
+# Go here for the original blog post and solutions
+# http://www.r-bloggers.com/k-means-clustering-from-r-in-action/
+
+# Exercise 0: Install these packages if you don't have them already
+
+# install.packages(c("cluster", "rattle.data","NbClust"))
+library("cluster")
+library("rattle.data")
+library("NbClust")
+
+# Now load the data and look at the first few rows
+data(wine, package="rattle.data")
+head(wine)
+
+# Exercise 1: Remove the first column from the data and scale
+# it using the scale() function
+df <- scale(wine[-1])
+
+# Now we'd like to cluster the data using K-Means. 
+# How do we decide how many clusters to use if you don't know that already?
+# We'll try two methods.
+
+# Method 1: A plot of the total within-groups sums of squares against the 
+# number of clusters in a K-means solution can be helpful. A bend in the 
+# graph can suggest the appropriate number of clusters. 
+
+wssplot <- function(data, nc=15, seed=1234){
+	              wss <- (nrow(data)-1)*sum(apply(data,2,var))
+               	      for (i in 2:nc){
+		        set.seed(seed)
+	                wss[i] <- sum(kmeans(data, centers=i)$withinss)}
+	                
+		      plot(1:nc, wss, type="b", xlab="Number of Clusters",
+	                        ylab="Within groups sum of squares")
+	   }
+
+wssplot(df)
+
+# Exercise 2:
+#   * How many clusters does this method suggest?
+#   Three, because this is where the curve bends from vertical to horizontal
+#   * Why does this method work? What's the intuition behind it?
+#   The vertical part of the curve represents a large reduction of within-cluster variance 
+#   as the number of clusters increases.  As the curve bends to the horizontal the reduction
+#   in within-cluster variance decreases (diminishing return on increasing the number of clusters)
+#   * Look at the code for wssplot() and figure out how it works
+#   wss is a vector of the within-group sum of squares for an increasing number of clusters.
+#   The first value in wss is the variance over the entire data set (everything in one cluster)
+#   A loop from 2 to the max number of clusters (nc) calculates the kmeans for a number of clusters
+#   and sums the values of the withinss column for that number of clusters
+#   The function plots the number of clusters along the x axis and wss on the y axis, with
+#   both dots and lines to show the curve and the wss value at each cluster count.
+
+# Method 2: Use the NbClust library, which runs many experiments
+# and gives a distribution of potential number of clusters.
+
+library(NbClust)
+set.seed(1234)
+nc <- NbClust(df, min.nc=2, max.nc=15, method="kmeans")
+barplot(table(nc$Best.n[1,]),
+	          xlab="Numer of Clusters", ylab="Number of Criteria",
+		            main="Number of Clusters Chosen by 26 Criteria")
+
+
+# Exercise 3: How many clusters does this method suggest?
+# Three
+
+# Exercise 4: Once you've picked the number of clusters, run k-means 
+# using this number of clusters. Output the result of calling kmeans()
+# into a variable fit.km
+
+
+# fit.km <- kmeans( ... )
+set.seed(1234)
+fit.km <- kmeans(df, 3, nstart = 25)
+fit.km$size
+fit.km$centers
+aggregate(wine[-1], by=list(cluster=fit.km$cluster), mean)
+
+# Now we want to evaluate how well this clustering does.
+
+# Exercise 5: using the table() function, show how the clusters in fit.km$clusters
+# compares to the actual wine types in wine$Type. Would you consider this a good
+# clustering?
+ct.km <- table(wine$Type, fit.km$cluster)
+ct.km
+library(flexclust)
+randIndex(ct.km)
+
+# Exercise 6:
+# * Visualize these clusters using  function clusplot() from the cluster library
+# * Would you consider this a good clustering?
+# This looks to be a good clustering based on the confusion table and on the cluster plot
+
+#clusplot( ... )
+clusplot(df, fit.km$cluster, main = 'Clusplot')
